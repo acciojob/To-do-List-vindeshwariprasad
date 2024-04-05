54e9a95176f7c8b0aa9984a2f52f74ae151292cc
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-04-05 10:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here
+document.addEventListener("DOMContentLoaded", () => {
+    let addTodoBtn = document.getElementById("addTodoBtn");
+
+    addTodoBtn.addEventListener("click", () => {
+        let newTodoInput = document.getElementById("newTodoInput");
+        let todoList = document.getElementById("todoList");
+        let newTodoValue = newTodoInput.value.trim();
+
+        if (newTodoValue !== "") {
+            let listItem = document.createElement("li");
+            listItem.textContent = newTodoValue;
+            todoList.appendChild(listItem);
+            newTodoInput.value = ""; // Clear the input field after adding todo
+        } else {
+            alert("Please enter a valid todo.");
+        }
+    });
+});
+
+
 
